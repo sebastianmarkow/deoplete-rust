0aef8e779c9d0eb374a4c86893bb210c96e81813
Sebastian Klatt <sebastian@markow.io>
2016-07-12 22:29:33 +0200

    Stage deoplete rust source

--- rplugin/python3/deoplete/sources/rust.py
+++ rplugin/python3/deoplete/sources/rust.py
@@ -1,5 +1,36 @@
+"""Rust completion via racer"""
+import re
+
 from .base import Base
+# from deoplete.util import error
 
 
 class Source(Base):
-    pass
+    """Deoplete Rust source"""
+
+    def __init__(self, vim):
+        Base.__init__(self, vim)
+
+        self.name = 'Rust'
+        self.mark = '[Rust]'
+        self.filetypes = ['go']
+        self.input_pattern = r'(\.|::)\w*'  # XXX(SK): words with digits?
+        self.rank = 500
+
+        self._racer = self.vim.funcs.executable(
+            self.vim.eval('deoplete#sources#rust#racer_binary')
+        )
+
+        self._rust_re = re.compile(r'\w*$')  # XXX(SK): words with digits?
+
+    def get_complete_position(self, ctx):
+        """Missing"""
+        if not self._racer:
+            return -1
+
+        method = self._rust_re.search(ctx['input'])
+        return method.start() if method else -1
+
+    def gather_candidates(self, ctx):
+        """Missing"""
+        pass
